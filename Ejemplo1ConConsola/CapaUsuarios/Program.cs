5dd990b194806a7685f71553958202d71b2dfb44
hugo <hugo@HUGO-PC>
2023-03-27 21:29:01 -0300

    Agregar archivos de proyecto.

---
+++ Ejemplo1ConConsola/CapaUsuarios/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaNegocios;
+
+namespace CapaUsuarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Persona pers, persOtra;
+            int d;
+            string n;
+            DateTime f;
+            Console.Write("Dni: ");
+            d = int.Parse(Console.ReadLine());
+            Console.Write("Nombre: ");
+            n = Console.ReadLine();
+            Console.Write("Fecha nacimiento: ");
+            f = DateTime.Parse(Console.ReadLine());
+            pers = new Persona(d, n, f);
+            Console.WriteLine("La edad de " + pers.Nombre + " es " + pers.edad());
+            persOtra = new Persona();
+            Console.Write("Dni: ");
+            d = int.Parse(Console.ReadLine());
+            Console.Write("Nombre: ");
+            n = Console.ReadLine();
+            Console.Write("Fecha nacimiento: ");
+            f = DateTime.Parse(Console.ReadLine());
+            persOtra.Dni = d;
+            persOtra.Nombre = n;
+            persOtra.FechaNac = f;
+            Console.WriteLine("La edad de " + persOtra.Nombre + " es " + persOtra.edad());
+        }
+    }
+}
